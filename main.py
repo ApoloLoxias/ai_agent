48a4d73fd31b16dc3883923f730949dcae69956d
ApoloLoxias <apololoxias@gmail.com>
2025-12-10 17:04:31 -0300

    Accepts user imput as argument to be passed as user_prompt to gemini

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,14 +11,23 @@ if not api_key:
 client = genai.Client(api_key = api_key)
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("user_prompt", type = str, help = "User prompt")
+    args = parser.parse_args()
+
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.5-flash', contents=args.user_prompt
     )
     if response is None:
         raise RuntimeError("response is None")
     if response.usage_metadata is None:
         raise RuntimeError("response.usage_metadata is None")
+    print("User prompt:")
+    print(args.user_prompt)
+    print("=======================")
+    print("Response:")
     print(response.text)
+    print("=======================")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
