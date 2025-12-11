16a71dd3f324929729bd93e71a652838f6cac862
ApoloLoxias <apololoxias@gmail.com>
2025-12-10 21:35:17 -0300

    cleaning up

--- main.py
+++ main.py
@@ -1,39 +1,51 @@
 import os
+import argparse
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-import argparse
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if not api_key:
-    raise RuntimeError("GEMNI_API_KEY variable not set on .env")
-
-client = genai.Client(api_key = api_key)
 
 def main():
+    client = initialise_client()
+    args = parse_arguments()
+    messages = [types.Content(role="user",parts=[types.Part(text=args.user_prompt)])]
+    response = generate_response(client, messages)
+    print_output(args.user_prompt, response, args.verbose)
+
+
+def initialise_client():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMNI_API_KEY variable not set on .env")
+    return genai.Client(api_key = api_key)
+
+
+def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("user_prompt", type = str, help = "User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
+def generate_response(client, messages):
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=messages
-    )
-    if response is None:
-        raise RuntimeError("response is None")
-    if response.usage_metadata is None:
-        raise RuntimeError("response.usage_metadata is None")
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
+        model='gemini-2.5-flash',
+        contents=messages,
+        )
+    if response is None or response.usage_metadata is None:
+        raise RuntimeError("Invalid Gemini response")
+    return response
+
+def print_output(user_prompt, response, verbose):
+    if verbose:
+        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         print("=======================")
     print("Response:")
     print(response.text)
-
 
 if __name__ == "__main__":
     main()
